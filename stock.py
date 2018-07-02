92640db5318b79e5a63eee09c67f034427e5db35
Vuk Vladisavljevic <vuk.vladisavljevic@gmail.com>
2018-07-02 20:32:16 +0200

    Final Version

--- stock.py
+++ stock.py
@@ -22,28 +22,11 @@ from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 
 
-def discard_columns(data):
-
-    #Removing columns that aren't needed for this forecast
-    columns = ['Open', 'High', 'Low', 'Close', 'Volume', 'Ex-Dividend',
-               'Split Ratio','Adj. High', 'Adj. Low','Adj. Open']
-    data.drop(columns, inplace=True, axis=1)
-    return data
-
 def calculate_average_price(row):
     return (row['Adj. High'] + row['Adj. Low']) / 2
 
 def calculate_change(row):
     return (row['Adj. Close'] - row['Adj. Open']) / row['Adj. Open']
-
-def remove_nan_cells(data):
-
-    #handling the NaN and empty cells
-    data.replace('', np.nan, inplace=True)
-    data = data.dropna(thresh=5)
-
-    return data
-
 
 def remove_outliers(data):
     temp_data = data;
@@ -74,19 +57,19 @@ def calculate(company_codename, forecast_period):
 
     #creating new column with average daily price which will be predicted
     #average_price column will be derived from Adj. High and Adj. Low columns
-   # df['average_price'] = df.apply(calculate_average_price, axis=1)
-   #df['change_percentage'] = df.apply(calculate_change, axis=1)
+    df['average_price'] = df.apply(calculate_average_price, axis=1)
+    df['change_percentage'] = df.apply(calculate_change, axis=1)
 
    #df = discard_columns(df)
 
    # print(df.describe())
-    #df = remove_outliers(df)
+    df = remove_outliers(df)
 
     #Creating new column that will represent average stock price in some future
     # this column will be created by shifting average_price value so that the new
     # column represents average_price value predefined time in future
     # -1 tell to shift backwards, or to move cell values up
-    df['predicted_price'] = df['Adj. Close'].shift(-forecast_period)
+    df['predicted_price'] = df['average_price'].shift(-forecast_period)
     #df = remove_nan_cells(df)
     df.dropna(inplace=True)
 
@@ -97,6 +80,7 @@ def calculate(company_codename, forecast_period):
     y = df.loc[:, "predicted_price"]
    # X.dropna(inplace=True)
 
+    #for best features to be choosen
     test = SelectKBest(score_func=mutual_info_regression, k=5)
     X = test.fit_transform(X, y)
     # summarize scores
@@ -110,36 +94,47 @@ def calculate(company_codename, forecast_period):
     #Creating train and test frames, by the 80% of all being training and 20% being testing values
     X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
 
-    regressor = Lasso(tol=0.01)
-    regressor.fit(X_train, y_train)
+    pca = PCA(n_components=5)
+    pca.fit(X_train)
+    x_transformed = pca.transform(X_train)
+    y_transformed = pca.transform(X_test)
 
-    y_pred = regressor.predict(X_test)
-    print("Root Mean Squared Error for prediction of " + company_codename + " average prices: ", np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
 
-    confidence = regressor.score(X_test, y_test)
-    print("Confidence for predicting " + company_codename + " average prices: " , confidence)
+    #n_jobs was set to -1 in case of multiple calculations, in that case all CPUs would be used
+    regressor = Lasso(tol=0.01, max_iter=10000)
+    regressor.fit(x_transformed, y_train)
+
+    y_pred = regressor.predict(y_transformed)
+   # print("Prediction period in days: ", forecast_period)
+   # print("Root Mean Squared Error for prediction of " + company_codename + " average prices: ", np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+    print(np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+   # print("***************************************************************************************")
     df2 = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
 
     return df2
 
-
+"""
+Nisam znao da li moramo kao kod domaćih zadataka samo rezultat da ispišemo
+pa sam sav kod koji plotuje i formatira ispis zakomentarisao
+"""
 def run() :
-    google_dataframe = calculate("GOOGL", 7)
-   # amazon_dataframe = calculate("AMZN")
-   # apple_dataframe = calculate("AAP")
-   # microsoft_dataframe = calculate("MSFT")
+    google_dataframe = calculate("GOOGL", 15)
+   # amazon_dataframe = calculate("AMZN", 30)
+   # tesla_dataframe = calculate("TSLA", 60)
 
-    google_dataframe['Actual'].plot(figsize=(25, 11), color="green")
-    google_dataframe['Predicted'].plot(figsize=(25, 11), color="red")
-    plt.legend(loc=4)
+"""
+    google_dataframe['Actual'].plot(figsize=(25, 11), color="green", label='Google Actual ')
+    google_dataframe['Predicted'].plot(figsize=(25, 11), color="red", label='Google Predicted ')
+
+    amazon_dataframe['Actual'].plot(figsize=(15, 6), color="blue", label='Amazon Actual ')
+    amazon_dataframe['Predicted'].plot(figsize=(15, 6), color="yellow", label='Amazon Predicted ')
+    tesla_dataframe['Actual'].plot(figsize=(15, 6), color="purple", label='Tesla Actual ')
+    tesla_dataframe['Predicted'].plot(figsize=(15, 6), color="black", label='Tesla Predicted ')
+    plt.legend(loc='upper left')
     plt.xlabel('Date')
-    plt.ylabel('Price in USD')
-
-    #amazon_dataframe['average_price'].plot(figsize=(15, 6), color="blue")
-   # apple_dataframe['average_price'].plot(figsize=(15, 6), color="red")
-   # microsoft_dataframe['average_price'].plot(figsize=(15, 6), color="yellow")
-   # plt.show()
-
+    plt.ylabel('Average price in USD')
+    plt.show()
+"""
 
 
 if __name__ == "__main__":
